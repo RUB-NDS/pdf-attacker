80033e850b6ccf1a62a91abedfa2c76535373ee9
Christian Mainka <Christian.Mainka@rub.de>
2020-06-18 19:14:47 +0200

    certifying working

--- default.nix
+++ default.nix
@@ -2,12 +2,14 @@ with import <nixpkgs> { };
 let
   my-python-packages = python-packages:
     with python-packages; [
+      oscrypto
+      pylint
       notebook
       scipy
     ];
   my-python = python37.withPackages my-python-packages;
 in pkgs.mkShell {
-  buildInputs = [ pkgs.python37Packages.pip my-python pkgs.swig ];
+  buildInputs = [ pkgs.python37Packages.pip my-python pkgs.swig pkgs.openssl.dev ];
   shellHook = ''
     alias pip="PIP_PREFIX='$(pwd)/_build/pip_packages' \pip"
     export PYTHONPATH="$(pwd)/_build/pip_packages/lib/python3.7/site-packages:$PYTHONPATH" 
