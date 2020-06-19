9d2b2c6d53e77bb3c78f6f7cd20500a0ce846224
Christian Mainka <Christian.Mainka@rub.de>
2020-06-19 11:11:06 +0200

    imagemagic + wand

--- default.nix
+++ default.nix
@@ -2,17 +2,23 @@ with import <nixpkgs> { };
 let
   my-python-packages = python-packages:
     with python-packages; [
-      oscrypto
       pylint
       notebook
-      scipy
     ];
   my-python = python37.withPackages my-python-packages;
 in pkgs.mkShell {
-  buildInputs = [ pkgs.python37Packages.pip my-python pkgs.swig pkgs.openssl.dev ];
+  buildInputs = [
+    pkgs.python37Packages.pip
+    my-python
+    pkgs.vscodium # IDE
+    pkgs.swig # dependency for: endesive
+    pkgs.openssl.dev # dependency for: endesive
+    pkgs.imagemagickBig # dependency for: wand
+  ];
   shellHook = ''
     alias pip="PIP_PREFIX='$(pwd)/_build/pip_packages' \pip"
     export PYTHONPATH="$(pwd)/_build/pip_packages/lib/python3.7/site-packages:$PYTHONPATH" 
     unset SOURCE_DATE_EPOCH
   '';
+  MAGICK_HOME = "${pkgs.imagemagickBig}";
 }
