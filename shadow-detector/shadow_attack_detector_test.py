f91d2bdf970342b6ff44fab25a0c14215140171d
Christian Mainka <Christian.Mainka@rub.de>
2020-08-20 09:12:38 +0000

    Created a TestSuite

--- shadow-detector/shadow_attack_detector_test.py
+++ shadow-detector/shadow_attack_detector_test.py
@@ -1,4 +1,4 @@
-from shadow_attack_detector import *
+from shadow_attack_detector import check_sig_state, start_preventor_detector, preventor, shadow_hide_and_hide_replace_detector, shadow_hide_form_detector, shadow_replace_font_detector, shadow_replace_form_detector
 import unittest
 
 class Test_ShadowAttackDetector(unittest.TestCase):
@@ -6,13 +6,42 @@ class Test_ShadowAttackDetector(unittest.TestCase):
         self.assertEqual(check_sig_state("./shadow-demo-exploits/hide/xref_image/hide-from-shadowed.pdf"), 0)
         self.assertEqual(check_sig_state("./shadow-demo-exploits/hide/xref_image/hide-from-shadowed-signed.pdf"), 1, "No Signature Found")
 
-    def test_shadow_hide_and_hide_replace_detector(self):
-        self.assertEqual(shadow_hide_and_hide_replace_detector("./shadow-demo-exploits/hide/xref_image/hide-from-shadowed-signed.pdf"), 0)
-        self.assertEqual(shadow_hide_and_hide_replace_detector("./shadow-demo-exploits/hide/xref_image/hide-from-shadowed-signed-manipulated_v1.pdf"), 1)
+    def test_shadow_hide_xref_image(self):
+        self.assertEqual(preventor("./shadow-demo-exploits/hide/xref_image/hide-from-shadowed.pdf"), 1)
+        self.assertEqual(preventor("./shadow-demo-exploits/hide/xref_image/hide-from-shadowed-signed.pdf"), 1)
+        self.assertEqual(preventor("./shadow-demo-exploits/hide/xref_image/hide-from-shadowed-signed-manipulated_v1.pdf"), 1)
         self.assertEqual(shadow_hide_and_hide_replace_detector("./shadow-demo-exploits/hide/xref_image/hide-from-shadowed-signed-manipulated_v2.pdf"), 1)
 
-    def test_shadow_hide_preventor(self):
-        self.assertEqual(shadow_hide_preventor("./shadow-demo-exploits/hide/xref_image/hide-from-shadowed.pdf"), 1)
+    def test_shadow_hide_form(self):
+        self.assertEqual(preventor("./shadow-demo-exploits/hide/form/hide-from-shadowed.pdf"), 2)
+        self.assertEqual(preventor("./shadow-demo-exploits/hide/form/hide-from-shadowed-signed.pdf"), 2)
+        self.assertEqual(shadow_hide_form_detector("./shadow-demo-exploits/hide/form/hide-from-shadowed-signed-manipulated.pdf"), 2)
+
+    def test_shadow_hide_form_and_text1(self):
+        self.assertEqual(preventor("./shadow-demo-exploits/hide/form_and_form/shadowed_form_over_form.pdf"), 2)
+        self.assertEqual(preventor("./shadow-demo-exploits/hide/form_and_form/shadowed_form_over_form-signed.pdf"), 2)
+        self.assertEqual(shadow_hide_form_detector("./shadow-demo-exploits/hide/form_and_form/shadowed_form_over_form-signed-manipulated_v1.pdf"), 0)
+        self.assertEqual(shadow_hide_form_detector("./shadow-demo-exploits/hide/form_and_form/shadowed_form_over_form-signed-manipulated_v2.pdf"), 0)
+        self.assertEqual(shadow_hide_and_hide_replace_detector("./shadow-demo-exploits/hide/form_and_form/shadowed_form_over_form-signed-manipulated_v1.pdf"), 0)
+        self.assertEqual(shadow_hide_and_hide_replace_detector("./shadow-demo-exploits/hide/form_and_form/shadowed_form_over_form-signed-manipulated_v2.pdf"), 0)
+
+    def test_shadow_hide_form_and_text2(self):
+        self.assertEqual(preventor("./shadow-demo-exploits/hide/form_and_text/form_over_text.pdf"), 2)
+        self.assertEqual(preventor("./shadow-demo-exploits/hide/form_and_text/form_over_text-signed.pdf"), 2)
+        self.assertEqual(shadow_hide_form_detector("./shadow-demo-exploits/hide/form_and_text/form_over_text-signed-manipulated.pdf"), 2)
+
+    def test_replace_font(self):
+        self.assertEquals(shadow_replace_font_detector("./shadow-demo-exploits/replace/font/replace-font-shadowed-signed-manipulated.pdf"), 1)
+
+    def test_replace_form(self):
+        self.assertEqual(preventor("./shadow-demo-exploits/replace/forms/replace-form-shadowed.pdf"), 1)
+        self.assertEqual(preventor("./shadow-demo-exploits/replace/forms/replace-form-shadowed-signed.pdf"), 1)
+        self.assertEqual(shadow_replace_form_detector("./shadow-demo-exploits/replace/forms/replace-form-shadowed-signed-manipulated.pdf"), 1)
+
+    def test_shadow_hide_and_replace(self):
+        self.assertEqual(preventor("./shadow-demo-exploits/hide-and-replace/hide-and-replace-shadowed.pdf"), 13)
+        self.assertEqual(preventor("./shadow-demo-exploits/hide-and-replace/hide-and-replace-shadowed-signed.pdf"), 15)
+        self.assertEqual(shadow_hide_and_hide_replace_detector("./shadow-demo-exploits/hide-and-replace/hide-and-replace-shadowed-signed-manipulated.pdf"), 14)
 
 class Test_ShadowAttackDetectorIntegration(unittest.TestCase):
     """
